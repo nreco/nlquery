e71234adde2cdf33ccf55dc2734cfddb4ace48bc
Vitaliy Fedorchenko <fedorchenko@gmail.com>
2024-02-08 17:16:31 +0200

    ComparisonMatcher: match condition in 'reverse' order + handle stop words inside the matcher #5

--- src/NReco.NLQuery/Matchers/ComparisonMatcher.cs
+++ src/NReco.NLQuery/Matchers/ComparisonMatcher.cs
@@ -27,18 +27,22 @@ namespace NReco.NLQuery.Matchers {
 
 		public IEnumerable<KeyValuePair<string[], ComparisonType>> PhraseComparisonTypes { get; set; }
 
+		public Func<string,bool> IsPhraseStopWord { get; set; }
+
 		public ComparisonMatcher(Func<Match,bool> leftPartPredicate, Func<Match,ComparisonType,Match, Match> getComparisonMatch) {
 			LeftPartPredicate = leftPartPredicate;
 			GetComparisonMatch = getComparisonMatch;
 		}
 
-		private bool MatchPhraseOp(Token[] tokens, ref int idx, out ComparisonType cmp) {
+		private bool MatchPhraseOp(Token[] tokens, ref int idx, out ComparisonType cmp, out int cmpTokensCount, bool goReverse = false) {
 			cmp = 0;
+			cmpTokensCount = 0;
 			foreach (var entry in PhraseComparisonTypes) {
 				if (entry.Key.Length>0) {
 					int startIdx = idx;
-					if (match(entry, ref startIdx)) {
+					if (goReverse ? matchInReverse(entry, ref startIdx) : match(entry, ref startIdx)) {
 						cmp = entry.Value;
+						cmpTokensCount = entry.Key.Length;
 						idx = startIdx;
 						return true;
 					}
@@ -51,12 +55,37 @@ namespace NReco.NLQuery.Matchers {
 					// skip separators
 					while (startIdx < tokens.Length && tokens[startIdx].Type==TokenType.Separator)
 						startIdx++;
-					if (tokens.Length<=startIdx || !String.Equals(entry.Key[i], tokens[startIdx].Value, StringComparison.OrdinalIgnoreCase))
+					if (tokens.Length<=startIdx || !String.Equals(entry.Key[i], tokens[startIdx].Value, StringComparison.OrdinalIgnoreCase)) { 
+						if (IsPhraseStopWord!=null && startIdx<tokens.Length && IsPhraseStopWord(tokens[startIdx].Value)) {
+							startIdx++; // skip stop word
+							i--; // check the same entry again
+							continue;
+						}
 						return false;
+					}
 					startIdx++;
 				}
 				return true;
 			}
+
+			bool matchInReverse(KeyValuePair<string[], ComparisonType> entry, ref int startIdx) {
+				for (int i = entry.Key.Length-1; i>=0; i--) {
+					// skip separators
+					while (startIdx >= 0 && tokens[startIdx].Type==TokenType.Separator)
+						startIdx--;
+					if (startIdx<=0 || !String.Equals(entry.Key[i], tokens[startIdx].Value, StringComparison.OrdinalIgnoreCase)) {
+						if (IsPhraseStopWord!=null && startIdx>=0 && IsPhraseStopWord(tokens[startIdx].Value)) {
+							startIdx--; // skip stop word
+							i++; // check the same entry again
+							continue;
+						}
+						return false;
+					}
+					startIdx--;
+				}
+				return true;
+			}
+
 
 		}
 
@@ -89,12 +118,14 @@ namespace NReco.NLQuery.Matchers {
 		}
 
 		public IEnumerable<Match> GetMatches(MatchBag matchBag) {
-			foreach (var cmpLeftPart in matchBag.Matches)
+			foreach (var cmpLeftPart in matchBag.Matches) {
 				if (LeftPartPredicate(cmpLeftPart)) {
 					var endIdx = matchBag.Statement.GetIndex(cmpLeftPart.End);
 					var tokens = matchBag.Statement.Tokens;
 					ComparisonType cmp = 0;
-					for (int i = endIdx + 1; i < tokens.Length-1 /* this should not be last token */ ; i++) {
+					int cmpTokens = 0;
+					// direct order: <entity> <op> <value> ("order year greater 2023")
+					for (int i = endIdx + 1; i < tokens.Length-1 /* op cannot be last token */ ; i++) {
 						var t = tokens[i];
 						switch (t.Type) {
 							case TokenType.Separator:
@@ -114,6 +145,7 @@ namespace NReco.NLQuery.Matchers {
 										hasMatches = true;
 										var mergedMatch = GetComparisonMatch(cmpLeftPart, cmp, m);
 										if (mergedMatch != null) {
+											mergedMatch.MatchedTokensCount = cmpLeftPart.MatchedTokensCount+cmpTokens+m.MatchedTokensCount;
 											mergedMatch.Start = cmpLeftPart.Start;
 											mergedMatch.End = m.End;
 											if (mergedMatch.Score == 0f) {
@@ -126,6 +158,7 @@ namespace NReco.NLQuery.Matchers {
 										// no matches, lets try to create comparison match by next token
 										var mergedMatch = GetComparisonMatch(cmpLeftPart, cmp, new StubMatch { Start = t, End = t });
 										if (mergedMatch != null) {
+											mergedMatch.MatchedTokensCount = cmpLeftPart.MatchedTokensCount+cmpTokens+mergedMatch.MatchedTokensCount;
 											mergedMatch.Start = cmpLeftPart.Start;
 											mergedMatch.End = t;
 											if (mergedMatch.Score == 0f)
@@ -135,15 +168,53 @@ namespace NReco.NLQuery.Matchers {
 									}
 								} else {
 									if (PhraseComparisonTypes != null)
-										if (MatchPhraseOp(tokens, ref i, out cmp))
+										if (MatchPhraseOp(tokens, ref i, out cmp, out cmpTokens))
 											continue; // next: read right part
 								}
 								break;
 						}
 						break; // stop
 					}
-				}
 
+					// reverse order: <op> <value> <entity> ("more than 5 items")
+					var leftPartPrevToken = matchBag.Statement.Prev(cmpLeftPart.Start, (t) => t.Type!=TokenType.Separator);
+					if (leftPartPrevToken!=null)
+						foreach (var cmpRightPart in matchBag.FindByEnd(leftPartPrevToken)) {
+							var rightPartPrevToken = matchBag.Statement.Prev(cmpRightPart.Start,
+									(t) => t.Type!=TokenType.Separator);
+							if (rightPartPrevToken==null)
+								continue;
+							var i = matchBag.Statement.GetIndex(rightPartPrevToken);
+							cmp = 0;
+							cmpTokens = 0;
+							switch (rightPartPrevToken.Type) {
+								case TokenType.Math:
+									while (i>0 && tokens[i-1].Type==TokenType.Math)
+										i--;
+									MatchMathOp(tokens, ref i, out cmp);
+									break;
+								case TokenType.Number:
+								case TokenType.Word:
+									if (PhraseComparisonTypes != null)
+										MatchPhraseOp(tokens, ref i, out cmp, out cmpTokens, goReverse:true);
+									break;
+							}
+							if (cmp>0) {
+								var mergedMatch = GetComparisonMatch(cmpLeftPart, cmp, cmpRightPart);
+								if (mergedMatch != null) {
+									mergedMatch.MatchedTokensCount = cmpLeftPart.MatchedTokensCount+cmpTokens+cmpRightPart.MatchedTokensCount;
+									mergedMatch.Start = tokens[i];
+									mergedMatch.End = cmpLeftPart.End;
+									if (mergedMatch.Score == 0f) {
+										mergedMatch.Score = (cmpLeftPart.Score + cmpRightPart.Score) / 2;
+									}
+									yield return mergedMatch;
+								}
+							}
+						}
+
+				}
+			}
 		}
 
 		public enum ComparisonType {
